3e50a19a70b9819d1086cd1f839d046c1b337bc3
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-25 16:14:14 -0500

    se crea estructura de carpetas

---
+++ src/main/java/com/example/userwebflux/UserWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.userwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userwebflux/UserWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
