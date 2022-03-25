83635dabfa25501638c5ce661a4a33ad7590f6b7
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-25 16:36:31 -0500

    se crea la coleccion User

--- src/main/java/com/example/userwebflux/domain/collection/User.java
+++ src/main/java/com/example/userwebflux/domain/collection/User.java
@@ -1,4 +1,19 @@
 package com.example.userwebflux.domain.collection;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@Document(value = "users")
 public class User {
+
+    @Id
+    private final String id;
+
+    private final String userName;
+    private final LocalDate timestamp;
+
 }
