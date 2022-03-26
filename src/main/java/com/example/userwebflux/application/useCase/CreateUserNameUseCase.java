f027ad07949f53bd4d55d79c339d394a63f99467
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-26 01:14:41 -0500

    se crea CreateUserNameUseCase

---
+++ src/main/java/com/example/userwebflux/application/useCase/CreateUserNameUseCase.java
@@ -0,0 +1,27 @@
+package com.example.userwebflux.application.useCase;
+
+import com.example.userwebflux.application.interfaces.CreateUserName;
+import com.example.userwebflux.application.mappers.UserNameMapper;
+import com.example.userwebflux.application.utils.RandomStringGenerator;
+import com.example.userwebflux.domain.dto.UserNameDto;
+import com.example.userwebflux.domain.repository.UserNameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CreateUserNameUseCase implements CreateUserName {
+
+    private final UserNameRepository userNameRepository;
+    private final UserNameMapper userNameMapper;
+    private final RandomStringGenerator randomStringGenerator;
+
+    @Override
+    public Mono<UserNameDto> createUserName(Integer length) {
+        return userNameRepository
+                .save(userNameMapper.mapToNewCollection()
+                        .apply(randomStringGenerator.generateString(length)))
+                .map(userNameMapper.mapToDto());
+    }
+}
--- src/main/java/com/example/userwebflux/domain/repository/UserNameRepository.java
+++ src/main/java/com/example/userwebflux/domain/repository/UserNameRepository.java
@@ -3,5 +3,5 @@ package com.example.userwebflux.domain.repository;
 import com.example.userwebflux.domain.collection.UserName;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface UserRespository extends ReactiveMongoRepository<UserName, String> {
+public interface UserNameRepository extends ReactiveMongoRepository<UserName, String> {
 }
