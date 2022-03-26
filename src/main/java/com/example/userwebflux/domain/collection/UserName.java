452c44da1cde433b78d01e0f0c6503a11e0db659
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-26 02:00:35 -0500

    se agregan campos a la base de datos

--- src/main/java/com/example/userwebflux/domain/collection/UserName.java
+++ src/main/java/com/example/userwebflux/domain/collection/UserName.java
@@ -13,6 +13,10 @@ public class UserName {
     @Id
     private final String id;
 
-    private final String value;
+    private final String userName;
+    private final String userEmail;
+    private final String userWasBorn;
+    private final String userPhone;
+    private final String userPassword;
     private final LocalDate timeStamp;
 }
